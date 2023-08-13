67ba0d6036cce7c63dd369ea08aab5f5e052f674
David Bradshaw <dabrad26@me.com>
2023-08-12 19:55:31 -0700

    fix: migrate off CRA

--- README.md
+++ README.md
@@ -5,10 +5,6 @@
 Runs the app in the development mode.
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.
-
 ### `npm run build`
 
 Builds the app for production to the `build` folder.
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+export default defineConfig(() => {
+  return {
+    server: {
+      port: 3000,
+      open: true,
+      https: true,
+    },
+    build: {
+      outDir: 'build',
+      assetsInclude: ['**/*.woff2'],
+    },
+    plugins: [
+      react(),
+      basicSsl(),
+    ],
+  };
+});
